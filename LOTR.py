cfcac015c4a9c025a9393d98c9e6f3ae896f9b69
jblazzy <blaz_j1@denison.edu>
2018-10-11 22:25:18 -0400

    Fleshed out some ideas

--- LOTR.py
+++ LOTR.py
@@ -1,10 +1,19 @@
 ## Josh Blaz -- LOTR
 
 import nltk
-
+import re
+import urllib.request
 
 
 
 text = open('LOTR.txt', 'r').read()
 
 ### Possibly split into chapters to do topic modeling on the topic of each chapter??
+
+## Silmarillion - https://archive.org/stream/fegmcfeggerson_gmail_4731/473%20%281%29_djvu.txt
+
+
+
+## Sentiment Analysis?
+## Overlay topic modeling over the sentiment analysis of each chapter
+    ## Train sentiment analysis using the Silmarillion maybe?
